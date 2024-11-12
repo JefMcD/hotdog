c93ae00bd30709b3d146d4ce094c6c234fce931d
JefMcD <artilleryvoo@protonmail.com>
2024-11-12 01:04:50 +0000

    pa host

--- hotdog_proj/settings.py
+++ hotdog_proj/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
